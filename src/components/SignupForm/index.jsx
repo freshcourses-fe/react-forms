08a86128de1fbbb6dcfeda0d1efa575e924c035e
Kirill <freshjs.ftd.@gmail.com>
2022-05-10 20:26:59 +0300

    Refactor styles

--- src/components/SignupForm/index.jsx
+++ src/components/SignupForm/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { SIGN_UP_CHEMA } from '../../utils/validationSchemas';
 import { Formik, Form, Field } from 'formik';
+import { SIGN_UP_CHEMA } from '../../utils/validationSchemas';
+import styles from  './SignupForm.module.scss';
 
 function signupUser(user) {
   alert('You are signed up!');
@@ -23,42 +24,30 @@ class SignupForm extends Component {
           passwordConfirm: '',
           email: '',
           isSubscribed: false,
-          userPlan: ''
+          userPlan: '',
         }}
         onSubmit={this.submitHandler}
       >
-        {(formikProps) => {
-          // console.log(formikProps);
-
-          return (
-            <Form
-              style={{
-                display: 'flex',
-                width: '300px',
-                flexDirection: 'column',
-                margin: '0 auto',
-                gap: '10px',
-              }}
-            >
-              <Field name="login" placeholder="Login" />
-              <Field name="password" placeholder="password" type="password" />
-              <Field
-                name="passwordConfirm"
-                placeholder="Repeat password"
-                type="password"
-              />
-              <Field name="email" type="email" placeholder="email" />
-              <Field name="userPlan" placeholder="Login" as="select">
-                <option value='basic'>Basic</option>
-                <option value='pro'>Pro</option>
-              </Field>
-              <label>
-                <Field name="isSubscribed" type="checkbox" /> Subscribe to news
-              </label>
-              <button type="submit">Sign Up</button>
-            </Form>
-          );
-        }}
+        <Form
+          className={styles.container}
+        >
+          <Field name="login" placeholder="Login" />
+          <Field name="password" placeholder="password" type="password" />
+          <Field
+            name="passwordConfirm"
+            placeholder="Repeat password"
+            type="password"
+          />
+          <Field name="email" type="email" placeholder="email" />
+          <Field name="userPlan" placeholder="Login" as="select">
+            <option value="basic">Basic</option>
+            <option value="pro">Pro</option>
+          </Field>
+          <label>
+            <Field name="isSubscribed" type="checkbox" /> Subscribe to news
+          </label>
+          <button type="submit">Sign Up</button>
+        </Form>
       </Formik>
     );
   }
