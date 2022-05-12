d88c772da4c7090fcb1ea4d832421af6d99f87aa
Kirill <freshjs.ftd.@gmail.com>
2022-05-12 18:56:49 +0300

    Add better error messages

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,12 +1,12 @@
 import * as yup from 'yup';
 
 export const SIGN_UP_CHEMA = yup.object({
-  login: yup.string().matches(/[\w!@#$%^]{4,16}/).required(),
+  login: yup.string().matches(/[\w!@#$%^]{4,16}/, 'Login must be 4 - 16 characters').required(),
   password: yup
     .string()
-    .matches(/^[A-Z][a-zA-Z0-9]{7,32}$/)
+    .matches(/^[A-Z][a-zA-Z0-9]{7,32}$/, 'password must be 8 - 32 characters long')
     .required(),
-  passwordConfirm: yup.string().oneOf([yup.ref('password')]),
+  passwordConfirm: yup.string().oneOf([yup.ref('password')], 'password must match'),
   email: yup.string().email().required(),
   isSubscribed: yup.boolean().required(),
 });
